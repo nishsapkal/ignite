b0f3bbcdd3e5a2e1ce08e5fbde56f908b881e3b7
Sapkal <nishikant.p.sapkal@accenture.com>
2023-03-29 07:50:02 +0800

    return list

--- customer/src/main/java/com/crm/customer/CustomerController.java
+++ customer/src/main/java/com/crm/customer/CustomerController.java
@@ -1,19 +1,18 @@
 package com.crm.customer;
 import com.crm.customer.orm.CustomerModel;
 import com.crm.customer.orm.CustomerService;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.websocket.server.PathParam;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -23,15 +22,31 @@ import java.util.stream.Collectors;
 public class CustomerController {
     @Autowired
     CustomerService customerService;
-    @GetMapping("/getCustomer")
-    public List<CustomerModel> getCustomers(){
+    @GetMapping("/customers")
+    @ResponseStatus(HttpStatus.OK)
+    public String getCustomers(){
         //System.out.println("Input customer id is :::"+ custId);
-        return customerService.getCustomerList();
-       /* return customerList
-                .stream()
-                .filter(q -> q.getCustomerId() ==custId)
-                .collect(Collectors.toList());*/
+      //  return customerService.getCustomerList();
+        String json = new Gson().toJson(customerService.getCustomerList());
+        return json;
 
-       // return Arrays.asList(matchingCustomer);
     }
+    @GetMapping("/customer")
+    @ResponseStatus(HttpStatus.OK)
+    public List<CustomerModel> returnCustomers(@PathParam("custId") Integer custId){
+        System.out.println("Input customer id is :::"+ custId);
+        List<CustomerModel> customerList=null;
+        if(custId !=null) {
+
+            return Optional.ofNullable( customerService.getCustomerList())
+                    .orElse(Collections.emptyList())
+                    .stream()
+                    .filter(q -> q.getCustomerId() == custId)
+                    .collect(Collectors.toList());
+        }else{
+            System.out.println("Please provide customerID");
+        }
+        return  customerList;
+    }
+
 }
