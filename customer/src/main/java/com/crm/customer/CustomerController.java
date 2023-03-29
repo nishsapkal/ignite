68aeeffb239b0ab696ff839b497999dc1dd4a801
Sapkal <nishikant.p.sapkal@accenture.com>
2023-03-29 10:13:45 +0800

    json change removed

--- customer/src/main/java/com/crm/customer/CustomerController.java
+++ customer/src/main/java/com/crm/customer/CustomerController.java
@@ -1,19 +1,17 @@
 package com.crm.customer;
 import com.crm.customer.orm.CustomerModel;
 import com.crm.customer.orm.CustomerService;
-import com.google.gson.Gson;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 import javax.websocket.server.PathParam;
 import java.util.*;
-import java.util.function.Predicate;
+
 import java.util.stream.Collectors;
 
 @RestController
@@ -24,11 +22,8 @@ public class CustomerController {
     CustomerService customerService;
     @GetMapping("/customers")
     @ResponseStatus(HttpStatus.OK)
-    public String getCustomers(){
-        //System.out.println("Input customer id is :::"+ custId);
-      //  return customerService.getCustomerList();
-        String json = new Gson().toJson(customerService.getCustomerList());
-        return json;
+    public List<CustomerModel> getCustomers(){
+        return customerService.getCustomerList();
 
     }
     @GetMapping("/customer")
